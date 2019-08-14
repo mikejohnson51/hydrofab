72f7cebcc813f98b764e8f852cb76ea8b845e128
David Blodgett <dblodgett@usgs.gov>
2019-08-13 19:20:00 -0500

    switch back to get_DM and clarify some column names.

--- R/match_flowpath.R
+++ R/match_flowpath.R
@@ -45,11 +45,11 @@ clean_geom <- function(x) {
 #' @param target_flowline sf data.frame with target flowlines and COMID or NHDPlusHR
 #' equivalent.
 #' @param hr_pair (advanced use) data.frame as output by get_hr_pair internal function.
-#' @param cores Will run search in parallel
 #' @export
 #' @importFrom sf st_join st_set_geometry st_within
 #' @importFrom tidyr unnest
 #' @importFrom dplyr select distinct  left_join bind_rows
+#' @importFrom nhdplusTools get_DM
 #' @examples
 #' library(dplyr)
 #' library(sf)
@@ -71,8 +71,7 @@ clean_geom <- function(x) {
 #' plot(hr_lp$geom, col = "black", add = TRUE)
 #'
 match_flowpaths <- function(source_flowline, target_catchment, target_flowline,
-                            hr_pair = NULL,
-                            cores = NULL) {
+                            hr_pair = NULL) {
 
   source_flowline <- nhdplusTools:::rename_nhdplus(source_flowline)
   check_names(source_flowline, "match_flowpaths")
@@ -85,6 +84,8 @@ match_flowpaths <- function(source_flowline, target_catchment, target_flowline,
   source_flowline <- select(source_flowline, required_names)
 
   target_flowline <- clean_geom(target_flowline)
+  
+  target_flowline <- select(target_flowline, NHDPlusID, HydroSeq, DnHydroSeq, LevelPathI, DnLevelPat)
 
   if(is.null(hr_pair)) {
     target_catchment <- nhdplusTools:::rename_nhdplus(target_catchment)
@@ -97,53 +98,34 @@ match_flowpaths <- function(source_flowline, target_catchment, target_flowline,
 
   gc()
 
-  hr_pair <- filter(hr_pair, FEATUREID %in% target_flowline$NHDPlusID) %>%
-    left_join(select(target_flowline, NHDPlusID, LevelPathI),
-              by = c("FEATUREID" = "NHDPlusID"))
-
-  target_fp <- select(target_flowline, "DnLevelPat", "LevelPathI", "HydroSeq") %>%
-    group_by(LevelPathI) %>%
-    filter(HydroSeq == min(HydroSeq)) %>%
-    select(-HydroSeq) %>%
-    ungroup()
-
-  get_dm_lp <- function(in_lp, fp) {
-    dn_lp <- fp$DnLevelPat[fp$LevelPathI %in% in_lp]
-
-    if(!is.null(dn_lp) && !is.na(dn_lp) && 
-       length(dn_lp) > 0 && in_lp != dn_lp) {
-      c(in_lp, get_dm_lp(dn_lp, fp))
-    } else {
-      in_lp
-    }
-  }
+  hr_pair <- rename(hr_pair, NHDPlusID = FEATUREID) %>%
+    filter(NHDPlusID %in% target_flowline$NHDPlusID)
   
-  target_flowline <- select(target_flowline, NHDPlusID, LevelPathI)
-
-  ### Trace down HR network for each.
-  mr_lps <- lapply(hr_pair$LevelPathI,
-                   get_dm_lp,
-                   fp = target_fp)
+  dm_NHDPlusID <- lapply(hr_pair$NHDPlusID,
+                         function(x, fa) get_DM(fa, x),
+                         fa = target_flowline)
+  
+  target_flowline <- select(target_flowline, NHDPlusID, hr_LevelPathI = LevelPathI)
 
   # Expand into data.frame
-  lp_df <- data.frame(COMID = hr_pair$COMID)
-  lp_df["member_hr_lp"] <- list(mr_lps)
-
-  rm(mr_lps)
+  lp_df <- data.frame(headwater_COMID = hr_pair$COMID)
+  lp_df["member_NHDPlusID"] <- list(dm_NHDPlusID)
 
   lp_df <- unnest(lp_df)
 
-  # Get MR levelpaths for headwater HR ids
-  hr_pair <- left_join(select(hr_pair, -LevelPathI), mr_lp, by = "COMID")
+  # Get MR levelpaths for headwater COMIDs
+  hr_pair <- left_join(hr_pair, 
+                       rename(mr_lp, mr_LevelPathI = LevelPathI), by = "COMID")
 
-  # Join so we have HR FEATUREID and MR LevelPath
-  lp_df <- left_join(lp_df, select(hr_pair, -FEATUREID), by = "COMID")
+  # Join so we have HR NHDPlusIDs found downstream of a given MR LevelPath headwater.
+  lp_df <- left_join(lp_df, select(hr_pair, -NHDPlusID), 
+                     by = c("headwater_COMID" = "COMID"))
 
-  group_by(lp_df, member_hr_lp) %>% # Group by level paths present in HR.
-    filter(LevelPathI == min(LevelPathI)) %>% # Filter so only one (largest) MR levelpath is linked to each HR path.
+  group_by(lp_df, member_NHDPlusID) %>% # Group by level paths present in HR.
+    filter(mr_LevelPathI == min(mr_LevelPathI)) %>% # Filter so only one (largest) MR levelpath is linked to each HR path.
     ungroup() %>%
     left_join(target_flowline,
-              by = c("member_hr_lp" = "LevelPathI"))
+              by = c("member_NHDPlusID" = "NHDPlusID"))
 }
 
 get_hr_pair <- function(mr_hw_outlets, target_catchment) {
--- tests/testthat/test_match_flowpaths.R
+++ tests/testthat/test_match_flowpaths.R
@@ -9,33 +9,29 @@ test_that("match flowpaths runs", {
 
   hr_flowline$TerminalPa <- 15000500003272
 
-  suppressWarnings(lp_df_df <- match_flowpaths(source_flowline = new_hope_flowline,
+  suppressWarnings(matched <- match_flowpaths(source_flowline = new_hope_flowline,
                                               target_catchment = hr_catchment,
                                               target_flowline = hr_flowline))
 
-  matched <- left_join(dplyr::select(hr_flowline, NHDPlusID),
-                           dplyr::select(lp_df_df, NHDPlusID,
-                                  MR_LevelPathI = LevelPathI), by = "NHDPlusID")
+  expect_equal(sum(!is.na(matched$mr_LevelPathI)), 1021)
 
-  expect_equal(sum(!is.na(matched$MR_LevelPathI)), 1205)
-
-  lp <- min(matched$MR_LevelPathI, na.rm = TRUE)
+  lp <- min(matched$mr_LevelPathI, na.rm = TRUE)
   mr_lp <- filter(new_hope_flowline, LevelPathI <= lp)
-  hr_lp <- filter(matched, MR_LevelPathI <= lp)
+  hr_lp <- filter(matched, mr_LevelPathI <= lp)
 
   # validated manually
   expect_equal(nrow(mr_lp), 75)
-  expect_equal(nrow(hr_lp), 146)
+  expect_equal(nrow(hr_lp), 139)
 })
 
-# Visualize match
-# lps <- unique(matched$MR_LevelPathI)
+# # Visualize match
+# lps <- unique(matched$mr_LevelPathI)
 # lps <- lps[!is.na(lps)]
 # for(lp in lps) {
 #   png(paste0("png/", lp, ".png"), width = 1024, height = 1024)
 #   par(mar = c(0, 0, 0, 0))
 #   mr_lp <- filter(new_hope_flowline, LevelPathI <= lp)
-#   hr_lp <- filter(matched, MR_LevelPathI <= lp)
+#   hr_lp <- filter(matched, mr_LevelPathI <= lp)
 #   plot(hr_flowline$geom, col = "blue", lwd = 0.5)
 #   plot(mr_lp$geom, col = "red", lwd = 3, add = TRUE)
 #   plot(hr_lp$geom, col = "black", add = TRUE)
