20bbc807eb44302ebced5ee98744ffabcb94de0b
David Blodgett <dblodgett@usgs.gov>
2019-09-07 20:43:30 -0500

    refactor match flowpaths and add handling for RF1

--- R/match_flowpath.R
+++ R/match_flowpath.R
@@ -1,23 +1,43 @@
+#' Get headwater points
+#' @description Returns headwater locations for line geometries 
+#' @param fline flowline data.frame from NHDPlus or with an ID and toID column.
 #' @importFrom sf st_sf st_coordinates st_as_sf st_crs
 #' @importFrom dplyr left_join select filter group_by ungroup bind_cols
 #' @importFrom nhdplusTools prepare_nhdplus
-mr_hw_cat_out <- function(mr_fline) {
-  mr_fline <- prepare_nhdplus(mr_fline, 0, 0, 0, warn = FALSE) %>%
-    left_join(select(mr_fline, COMID), by = "COMID") %>%
-    st_sf() %>%
-    filter(!COMID %in% toCOMID)
+#' @export
+get_hw_points <- function(fline) {
+  if("COMID" %in% names(fline)) {
+    fline <- prepare_nhdplus(fline, 0, 0, 0, warn = FALSE) %>%
+      left_join(select(fline, COMID), by = "COMID") %>%
+      st_sf() %>%
+      filter(!COMID %in% toCOMID)
+  } else {
+    fline <- filter(fline, !ID %in% toID)
+  }
 
-  outlets <-  mr_fline %>%
+  outlets <-  fline %>%
     st_coordinates() %>%
-    as.data.frame() %>%
-    group_by(L2) %>%
+    as.data.frame()
+  
+  if("L2" %in% names(outlets)) {
+    outlets <- group_by(outlets, L2)
+  } else {
+    outlets <- group_by(outlets, L1)
+  }
+  
+  outlets <- outlets %>%
     filter(row_number() == round(n()/2)) %>%
     ungroup() %>%
     select(X, Y) %>%
     st_as_sf(coords = c("X", "Y"))
 
-  bind_cols(outlets, select(st_set_geometry(mr_fline, NULL), COMID)) %>%
-    st_sf(crs = st_crs(mr_fline))
+  if("COMID" %in% names(fline)) {
+    bind_cols(outlets, select(st_set_geometry(fline, NULL), COMID)) %>%
+      st_sf(crs = st_crs(fline))
+  } else {
+    bind_cols(outlets, select(st_set_geometry(fline, NULL), ID)) %>%
+      st_sf(crs = st_crs(fline))
+  }
 }
 
 clean_geom <- function(x) {
@@ -35,17 +55,13 @@ clean_geom <- function(x) {
 #' location of the upstream most catchment in the source flowlines to stay away from
 #' complexity that occurs near drainage divides.
 #'
-#' For preformance reasons, network navigation only considers the network of levelpaths.
-#' As a result, the response includes all flowlines along headwater levelpaths,
-#' including those upstream of the outlet of the headwater catchment.
-#'
-#' @param source_flowline sf data.frame with source flowlines and flowline attributes:
-#' COMID, LENGTHKM, DnHydroseq, and Hydroseq, and LevelPathI or NHDPlusHR equivalents.
-#' @param target_catchment sf data.frame with catchment polygons and FEATUREID or
-#' NHDPlusHR equivalent.
-#' @param target_flowline sf data.frame with target flowlines and COMID or NHDPlusHR
-#' equivalent.
-#' @param hr_pair (advanced use) data.frame as output by get_hr_pair internal function.
+#' @param source_flowline sf data.frame with COMID and LevelPathI attributes.
+#' @param target_flowline sf data.frame either NHDPlusHR with NHDPlusID, LENGTHKM, DnHydroseq, 
+#' Hydroseq, and LevelPathI or an ad-hoc network with ID and toID attributes.
+#' @param hw_pair data.frame with headwater pairs
+#' @return data.frame with the headwater id from the source_flowline input and 
+#' levelpath from the source_flowline input for each identifier matched from the 
+#' target dataset.
 #' @export
 #' @importFrom sf st_join st_set_geometry st_within
 #' @importFrom tidyr unnest
@@ -55,9 +71,12 @@ clean_geom <- function(x) {
 #' source(system.file("extdata/nhdplushr_data.R", package = "hyRefactor"))
 #' source(system.file("extdata/new_hope_data.R", package = "nhdplusTools"))
 #'
-#' lp_df_df <- match_flowpaths(source_flowline = new_hope_flowline,
-#'                            target_catchment = hr_catchment,
-#'                            target_flowline = hr_flowline)
+#' hr_catchment <- nhdplusTools:::rename_nhdplus(hr_catchment)
+#' hw_pair <- sf::st_set_geometry(sf::st_join(get_hw_points(new_hope_flowline),
+#'                               dplyr::select(hr_catchment, FEATUREID),
+#'                               join = sf::st_within), NULL)
+#'
+#' lp_df_df <- match_flowpaths(new_hope_flowline, hr_flowline, hw_pair)
 #' matched <- dplyr::left_join(dplyr::select(hr_flowline, NHDPlusID),
 #'                             dplyr::select(lp_df_df, member_NHDPlusID,
 #'                                           MR_LevelPathI = mr_LevelPathI), 
@@ -70,68 +89,109 @@ clean_geom <- function(x) {
 #' plot(sf::st_geometry(mr_lp), col = "red", lwd = 3, add = TRUE)
 #' plot(sf::st_geometry(hr_lp), col = "black", add = TRUE)
 #'
-match_flowpaths <- function(source_flowline, target_catchment, target_flowline,
-                            hr_pair = NULL) {
+match_flowpaths <- function(source_flowline, target_flowline, hw_pair) {
 
   source_flowline <- nhdplusTools:::rename_nhdplus(source_flowline)
-  check_names(source_flowline, "match_flowpaths")
-
-  required_names <- unique(c(get("match_flowpaths_attributes",
-                                 hyRefactor_env),
-                             get("prepare_nhdplus_attributes",
-                                 hyRefactor_env)))
-
-  source_flowline <- select(source_flowline, required_names)
-
+  source_flowline <- clean_geom(source_flowline)
   target_flowline <- clean_geom(target_flowline)
   
-  target_flowline <- select(target_flowline, NHDPlusID, HydroSeq, DnHydroSeq, LevelPathI, DnLevelPat)
+  if("ID" %in% names(target_flowline)) {
+    
+    lpt <- data.frame(headwater_COMID = hw_pair$FEATUREID)
+    
+    lpt["member_ID"] <- list(lapply(hw_pair$ID,
+                                      function(x, fa) get_dwn(x, fa),
+                                      fa = target_flowline))
+    
+    lpt <- unnest(lpt)
+    
+    lps <- data.frame(headwater_COMID = hw_pair$FEATUREID)
+    
+    lps["member_COMID"] <- list(lapply(hw_pair$FEATUREID,
+                                             function(x, fa) get_DM(fa, x),
+                                             fa = source_flowline))
+    
+    lps <- unnest(lps)
+    
+    lps <- lps %>%
+      left_join(select(source_flowline, LevelPathI, Hydroseq, COMID), 
+                by = c("member_COMID" = "COMID")) %>%
+      group_by(LevelPathI) %>%
+      filter(Hydroseq == max(Hydroseq)) %>% 
+      ungroup() %>% group_by(headwater_COMID) %>%
+      filter(LevelPathI == min(LevelPathI)) %>%
+      select(-Hydroseq, mr_LevelPathI = LevelPathI, -member_COMID) %>%
+      ungroup()
+    
+    target_flowline <- select(clean_geom(target_flowline), ID)
+    source_flowline <- distinct(select(source_flowline, COMID, LevelPathI))
+    
+    gc()
+    
+    hw_pair <- hw_pair %>%
+      rename(COMID = FEATUREID) %>%
+      left_join(lps, by = c("COMID" = "headwater_COMID"))
+    
+    # Join so we have RF1 IDs found downstream of a given MR LevelPath headwater.
+    lpt <- left_join(lpt, select(hw_pair, -ID),
+                     by = c("headwater_COMID" = "COMID"))
+    
+    group_by(lpt, member_ID) %>%
+      filter(mr_LevelPathI == min(mr_LevelPathI)) %>%
+      ungroup()
+      
+  } else {
 
-  if(is.null(hr_pair)) {
-    target_catchment <- nhdplusTools:::rename_nhdplus(target_catchment)
-    hr_pair <- get_hr_pair(mr_hw_cat_out(source_flowline), target_catchment)
-  }
-
-  source_flowline <- clean_geom(source_flowline)
-  mr_lp <- distinct(select(source_flowline, COMID, LevelPathI))
-  rm(source_flowline)
-
+  target_flowline <- select(clean_geom(target_flowline),
+                            NHDPlusID, HydroSeq, DnHydroSeq, 
+                            LevelPathI, DnLevelPat)
+  
   gc()
 
-  hr_pair <- rename(hr_pair, NHDPlusID = FEATUREID) %>%
+  hw_pair <- rename(hw_pair, NHDPlusID = FEATUREID) %>%
     filter(NHDPlusID %in% target_flowline$NHDPlusID)
   
-  dm_NHDPlusID <- lapply(hr_pair$NHDPlusID,
+  dm_NHDPlusID <- lapply(hw_pair$NHDPlusID,
                          function(x, fa) get_DM(fa, x),
                          fa = target_flowline)
   
-  target_flowline <- select(target_flowline, NHDPlusID, hr_LevelPathI = LevelPathI)
+  target_flowline <- select(target_flowline, NHDPlusID)
 
   # Expand into data.frame
-  lp_df <- data.frame(headwater_COMID = hr_pair$COMID)
+  lp_df <- data.frame(headwater_COMID = hw_pair$COMID)
   lp_df["member_NHDPlusID"] <- list(dm_NHDPlusID)
 
   lp_df <- unnest(lp_df)
 
   # Get MR levelpaths for headwater COMIDs
-  hr_pair <- left_join(hr_pair, 
-                       rename(mr_lp, mr_LevelPathI = LevelPathI), by = "COMID")
+  hw_pair <- left_join(hw_pair, 
+                       rename(source_flowline, mr_LevelPathI = LevelPathI), 
+                       by = "COMID")
 
   # Join so we have HR NHDPlusIDs found downstream of a given MR LevelPath headwater.
-  lp_df <- left_join(lp_df, select(hr_pair, -NHDPlusID), 
+  lp_df <- left_join(lp_df, select(hw_pair, -NHDPlusID), 
                      by = c("headwater_COMID" = "COMID"))
 
   group_by(lp_df, member_NHDPlusID) %>% # Group by level paths present in HR.
     filter(mr_LevelPathI == min(mr_LevelPathI)) %>% # Filter so only one (largest) MR levelpath is linked to each HR path.
     ungroup() %>%
     left_join(target_flowline,
-              by = c("member_NHDPlusID" = "NHDPlusID"))
+              by = c("member_NHDPlusID" = "NHDPlusID")) %>%
+    select(-headwater_COMID)
+  }
 }
 
-get_hr_pair <- function(mr_hw_outlets, target_catchment) {
-  ### Find HR catchment of outlet of headwater MR catchments
-  hr_pair <- st_join(mr_hw_outlets,
-                     select(target_catchment, FEATUREID),
-                     join = st_within) %>%
-    st_set_geometry(NULL)
+get_dwn_rec <- function(ID, target_flowline) {
+  next_dn <- target_flowline$toID[target_flowline$ID == ID]
+  if(is.na(next_dn)) return()
+  c(ID, get_dwn(next_dn, target_flowline))
+}
+
+get_dwn <- function(ID, target_flowline) {
+  next_dn <- target_flowline$toID[target_flowline$ID == ID]
+  if(is.na(next_dn)) {
+    return(ID)
+  } else {
+    return(get_dwn_rec(ID, target_flowline))
+  }
 }
--- tests/testthat/test_match_flowpaths.R
+++ tests/testthat/test_match_flowpaths.R
@@ -2,23 +2,29 @@ context("match flowpaths")
 
 test_that("match flowpaths runs", {
   source(system.file("extdata/nhdplushr_data.R", package = "hyRefactor"))
-
+  
   source(system.file("extdata/new_hope_data.R", package = "nhdplusTools"))
-
+  
   rm(list = ls()[!ls() %in% c("new_hope_flowline", "hr_catchment", "hr_flowline")])
-
+  
   hr_flowline$TerminalPa <- 15000500003272
-
+  
+  hr_catchment <- nhdplusTools:::rename_nhdplus(hr_catchment)
+  suppressWarnings(hw_pair <- sf::st_join(get_hw_points(new_hope_flowline),
+                                          dplyr::select(hr_catchment, FEATUREID),
+                                          join = sf::st_within) %>%
+                     st_set_geometry(NULL))
+  
   suppressWarnings(matched <- match_flowpaths(source_flowline = new_hope_flowline,
-                                              target_catchment = hr_catchment,
-                                              target_flowline = hr_flowline))
-
+                                              target_flowline = hr_flowline, 
+                                              hw_pair = hw_pair))
+  
   expect_equal(sum(!is.na(matched$mr_LevelPathI)), 1021)
-
+  
   lp <- min(matched$mr_LevelPathI, na.rm = TRUE)
   mr_lp <- filter(new_hope_flowline, LevelPathI <= lp)
   hr_lp <- filter(matched, mr_LevelPathI <= lp)
-
+  
   # validated manually
   expect_equal(nrow(mr_lp), 75)
   expect_equal(nrow(hr_lp), 139)
@@ -37,4 +43,46 @@ test_that("match flowpaths runs", {
 #   plot(hr_lp$geom, col = "black", add = TRUE)
 #   dev.off()
 # }
+
+test_that("match flowpaths runs with RF1 and V2", {
+  hw_pairs <- structure(list(ID = c(7849L, 7851L, 7853L, 7856L, 7857L, 7858L, 
+                                    7860L, 7862L, 7863L, 7864L), 
+                             FEATUREID = c(2171225L, 2171903L, 2171013L, 
+                                           2171859L, 2171883L, 2171917L, 
+                                           2172119L, 2171999L, 2172059L, 
+                                           2171563L)), 
+                        row.names = c(NA, -10L), 
+                        class = c("tbl_df", "tbl", "data.frame"))
+  rf1 <- sf::read_sf(system.file("extdata/rf1_test.gpkg", package = "hyRefactor"), "rf1_test")
+  nhdp <- sf::read_sf(system.file("extdata/rf1_test.gpkg", package = "hyRefactor"), "nhdp_flowline")
+  
+  rf1_matches <- match_flowpaths(nhdp, rf1, hw_pairs)
+  
+  expect_equal(unique(rf1_matches$mr_LevelPathI[rf1_matches$headwater_COMID == 	2171013]), 290008321)
+})
+
+
+# viz results
+# mrlp <- unique(rf1_matches$mr_LevelPathI)
+# 
+# plot_fun <- function(step, rf1, nhdp, mrlp) {
+#   par(mar = c(0,0,0,0))
+#   plot(st_geometry(rf1), lwd = 0.5)
+#   if(step > 1) {
+#     if(step %% 2 == 0) {
+#       lps <- mrlp[1:(step/2)]
+#     } else {
+#       lps <- mrlp[1:((step-1)/2)]
+#     }
+#     plot(st_geometry(filter(nhdp, LevelPathI %in% lps)), lwd = 2, add =TRUE)
+#     if(step %% 2 == 0) lps <- lps[1:(length(lps) - 1)]
+#     
+#     if(step > 2) 
+#       plot(st_geometry(filter(rf1, ID %in% filter(rf1_matches, mr_LevelPathI %in% lps)$member_ID)), col = "red", add = TRUE)
+#   }
+# }
+# 
+# gifski::save_gif(lapply(1:(length(mrlp) * 2), plot_fun, 
+#                         rf1 = rf1, nhdp = nhdp, mrlp = mrlp))
+
 
